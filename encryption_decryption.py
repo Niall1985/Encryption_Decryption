eb4060d7a3801b8253994a460ceb05ade6a963d2
Niall Dcunha <152503679+Niall1985@users.noreply.github.com>
2024-04-30 01:57:01 +0530

    Update encryption_decryption.py

--- encryption_decryption.py
+++ encryption_decryption.py
@@ -1,37 +1,44 @@
 import string
 import random
 import sys
-chars = " " + string.ascii_letters + string.digits + string.punctuation
-chars = list(chars)
-keys = chars.copy()
-random.shuffle(keys)
+dict={}
+def generate_keys():
+    chars = " " + string.ascii_letters + string.digits + string.punctuation
+    chars = list(chars)
+    keys = chars.copy()
+    random.shuffle(keys)
+    return keys
 
 def program_run():
-    user_input = input("Enter [encryption/decryption/exit]:").lower()
-    if user_input == "encryption":
-        plain_text()
-    elif user_input == "decryption":
+    keys = generate_keys()
+    chars = generate_keys()
+    User_input=input("Enter E to proceed to Encryption, D to proceed to Decryption and Q to exit:")
+    if User_input == 'E':
+        plain_text(keys,chars)
+    elif User_input == 'D':
         cipher_text()
-    elif user_input == "exit":
+    elif User_input == 'Q':
         sys.exit(0)
+    else:
+        print("Invalid Input")
 
-def plain_text():
+def plain_text(keys,chars):
     plain_text=input("Enter text to be encrypted:")
     cipher_text=""
     for letters in plain_text:
         index = chars.index(letters)
         cipher_text += keys[index]
+        dict[plain_text]=cipher_text
     print(f"Plain Text:{plain_text}")
     print(f"Cipher Text:{cipher_text}")
 
 def cipher_text():
-    cipher_text=input("Enter text to be decrypted:")
-    plain_text=""
-    for letters in cipher_text:
-        index = keys.index(letters)
-        plain_text += chars[index]
-    print(f"Cipher Text:{cipher_text}")
-    print(f"Plain Text:{plain_text}")
+    cipher_text=input("Enter the Encrypted code which you want to Decrypt:")
+    for key, value in dict.items():
+        if value == cipher_text:
+            print("Decrypted Message:",key)
+        else:
+            print("Incorrect Encryption input")
 
 while True:
     program_run()
